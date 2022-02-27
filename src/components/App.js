1d05af9567a342f90b3b6817ca307bb0f18abffa
Sisi Vieira <cicy886@gmail.com>
2022-02-27 14:33:05 -0800

    Fetch data from api url

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,26 @@
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const API_URL ='https://rws-cards-api.herokuapp.com/api/v1/cards/';
+  const [reqType, setReqType] =useState('');
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch (`${API_URL}${reqType}`);
+        const data = await response.json();
+        setCards(data);
+      }catch (error) {
+        <div>Error: {error.message}</div>
+      }
+    }
+
+    fetchCards();
+
+  },[reqType])
+
   return (
     <div className="App">
       <h3>App</h3>
