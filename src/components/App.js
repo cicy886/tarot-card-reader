e4ff162cc54f5255426b22ccf7bce0d2bbb9becd
Sisi Vieira <cicy886@gmail.com>
2022-03-01 19:18:09 -0800

    Add popup functionality and pass it as a prop to the cardDetailPopup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import CardList from './CardList';
+import React, { useState, useEffect } from "react";
+import CardList from "./CardList";
+import CardDetailPopup from "./CardDetailPopup";
 
 function App() {
-
-  const API_URL ='https://rws-cards-api.herokuapp.com/api/v1/cards/';
+  const API_URL = "https://rws-cards-api.herokuapp.com/api/v1/cards/";
 
   // reqType use for random route
-  const [reqType, setReqType] =useState('');
+  const [reqType, setReqType] = useState("");
   const [cards, setCards] = useState([]);
   const [selectedCard, setSelectedCard] = useState(null);
+  const [popupTrigger, setPopupTrigger] = useState(false);
 
   useEffect(() => {
     const fetchCards = async () => {
       try {
-        const response = await fetch (`${API_URL}${reqType}`);
+        const response = await fetch(`${API_URL}${reqType}`);
         const data = await response.json();
         setCards(data);
-      }catch (error) {
-        <div>Error: {error.message}</div>
+      } catch (error) {
+        <div>Error: {error.message}</div>;
       }
-    }
+    };
 
     fetchCards();
-
-  },[reqType])
+  }, [reqType]);
 
   const handleChangingSelectedCard = (id) => {
     const selectedCard = cards.cards[id];
     setSelectedCard(selectedCard);
+    setPopupTrigger(true);
   };
 
   return (
     <div className="App">
-      {Object.values(cards).map((card, index) =>
-      <CardList 
-        card = {card}
-        key = {index}
-        />
-      )}
+      {Object.values(cards).map((card, index) => (
+        <CardList card={card} key={index} />
+      ))}
+
+      <CardDetailPopup
+        popupTrigger={popupTrigger}
+        setPopupTrigger={setPopupTrigger}
+        selectedCard={selectedCard}
+      />
     </div>
   );
 }
