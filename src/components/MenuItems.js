d0f61ccd253c92c9889642330fd5e81536854dd7
Sisi Vieira <cicy886@gmail.com>
2022-03-10 19:12:33 -0800

    Add navbar and dropdown list

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Wands',
+    path: '/wands',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Cups',
+    path: '/cups',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Swords',
+    path: '/swords',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Pentacles',
+    path: '/Pentacles',
+    cName: 'dropdown-link'
+  },
+  ]
